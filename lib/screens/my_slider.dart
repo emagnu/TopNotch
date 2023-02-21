3441b87771666790460f3901852011db395553a5
Emmanuele Aggugini-Levi <emagnubcp@gmail.com>
2023-02-21 13:30:05 +0000

    all: Make Website in Flutter | Creating Carousel Slider with Flutter Web | Flutter Web Part # 2

--- lib/screens/my_slider.dart
+++ lib/screens/my_slider.dart
@@ -16,6 +16,8 @@ class MySlider extends StatefulWidget {
 class _MySliderState extends State<MySlider> {
   // Controller
   CarouselController controller = CarouselController();
+  // Image counter
+  int currentImg = 0;
   // Image list
   var imageList = [
     'mingchow01.jpg',
@@ -71,12 +73,56 @@ class _MySliderState extends State<MySlider> {
                         color: Colors.white,
                       ),
                     ),
-                  )
+                  ),
+                  Positioned(
+                    bottom: 0.0,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width, // * 0.45,
+                      height: 50.0, color: const Color(0xFF27AE60),
+                      child: Center(
+                        child: SizedBox(
+                          width: 300.0,
+                          height: 5.0,
+                          child: ListView.builder(
+                            itemCount: imageList.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                onTap: () {
+                                  controller.jumpToPage(index);
+                                },
+                                child: Container(
+                                  width: 30.0,
+                                  height: 5.0,
+                                  margin: const EdgeInsets.only(right: 10.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: currentImg == index
+                                        ? Colors.white
+                                        : Colors.white.withOpacity(0.5),
+                                  ),
+                                  // child: Widget(),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             )
             .toList(),
-        options: CarouselOptions(autoPlay: true, viewportFraction: 1.0),
+        options: CarouselOptions(
+          autoPlay: true,
+          viewportFraction: 1.0,
+          onPageChanged: (index, reason) {
+            setState(() {
+              currentImg = index;
+            });
+          },
+        ),
       ),
     );
   }
